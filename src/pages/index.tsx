2dd9fe695d68c36ce361f1a72934c9cf8396a980
stanley <stanleygomesdasilva@gmail.com>
2025-08-08 09:43:48 -0300

    chore: redirecionando a home para stories

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,48 +1,6 @@
 import React from 'react';
-import type { ReactNode } from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import Heading from '@theme/Heading';
+import { Redirect } from '@docusaurus/router';
 
-import styles from './index.module.css';
-
-function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className='container'>
-        <Heading as='h1' className='hero__title'>
-          {siteConfig.title}
-        </Heading>
-        <p className='hero__subtitle'>{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className='button button--secondary button--lg'
-            to='/docs/intro'
-          >
-            Comece por aqui
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
-
-export default function Home(): ReactNode {
-  return (
-    <Layout
-      title={`A Graça, o perdão e o sacrifício`}
-      description='Embarque nessa jornada de aprendizado sobre a história Daquele que dividiu a história em antes e depois.'
-    >
-      <HomepageHeader />
-      Embarque nessa jornada de aprendizado sobre a história Daquele que dividiu
-      a história em antes e depois.
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
-  );
+export default function Home() {
+  return <Redirect to='/historias' />;
 }
